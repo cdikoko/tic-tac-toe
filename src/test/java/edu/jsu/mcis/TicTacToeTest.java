9553290d7650d19cad15ae0aa37c2239afe20db6
cdikoko <cdikoko@stu.jsu.edu>
2016-09-07 23:50:48 -0500

    First Commit

---
+++ src/test/java/edu/jsu/mcis/TicTacToeTest.java
@@ -0,0 +1,61 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TicTacToeTest {
+	public TicTacToe model;
+	
+	@Before
+	public void setup(){
+		model = new TicTacToe();
+	}
+		
+	@Test
+	public void testInitialBoardIsEmpty (){
+			model.clear(); 
+			}
+		
+	
+	@Test
+	public void testMarkXInUpperRightCorner() {
+		model.getMove(0,2);
+		
+	}
+		
+	
+	@Test
+	public void testMarkXinCenter(){
+		assertTrue(false);
+	}
+
+	@Test
+	public void testMarkXinCenterThenOInCenter(){
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testMarkOInBottomLeftCorner() {
+	assertTrue(false);	
+	}
+	
+	@Test
+	public void testUnableToMarkOverExistingMark() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testGameIsNotOverAfterTheFirstMark() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testGameIsWonByXHorizontallyAcrossTopRow() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testGameIsOverByTieIfAllLocationsAreFilled() {
+		assertTrue(false);
+	}	
+}
